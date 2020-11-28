75a900758eb87bf07dd0dcb99d964ce21ed4f35d
Hexadecane <36948730+Hexadecane@users.noreply.github.com>
2020-11-28 09:14:00 -0500

    Fixes warning with SDL_SetError in util.cpp

--- src/util.cpp
+++ src/util.cpp
@@ -3,6 +3,6 @@
 // Utility functions. util.h should generally be included in all of the game's header files as boilerplate.
 
 void ThrowError(std::string msg) {
-	SDL_SetError(msg.c_str());
+	SDL_SetError("SDL_Error: %s", msg.c_str());
 	std::cerr << "SDL_Error: " << SDL_GetError() << '\n';
 }
