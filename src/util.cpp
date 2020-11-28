8e851aa819badb8822c414306ac15b7de23bcca6
Hexadecane <36948730+Hexadecane@users.noreply.github.com>
2020-11-28 09:01:46 -0500

    Update util.cpp

--- src/util.cpp
+++ src/util.cpp
@@ -3,7 +3,6 @@
 // Utility functions. util.h should generally be included in all of the game's header files as boilerplate.
 
 void ThrowError(std::string msg) {
-	//std::cerr << msg << '\n';
 	SDL_SetError(msg.c_str());
 	std::cerr << "SDL_Error: " << SDL_GetError() << '\n';
 }
